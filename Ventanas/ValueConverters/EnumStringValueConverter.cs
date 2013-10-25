d4f97ff97bac83a8c8986318d66a129282837c8c
tborres <tborres@base2.io>
2013-10-25 17:10:27 -0400

    #1 - Show the enum string values in a list box.

---
+++ Ventanas/ValueConverters/EnumStringValueConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Base2io.Util.EnumUtil;
+
+namespace Base2io.Ventanas.ValueConverters
+{
+    class EnumStringValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var valueAsEnum = value as Enum;
+            return valueAsEnum != null ? valueAsEnum.GetStringValue() : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
