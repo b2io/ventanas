ce1c9ec21e8520e9f44d6de41a9a636cba12e556
tborres <tborres@base2.io>
2013-10-25 17:33:12 -0400

    #1 - Display hotkey list as listview.

--- Ventanas/Enums/WindowPosition.cs
+++ Ventanas/Enums/WindowPosition.cs
@@ -7,7 +7,7 @@ namespace Base2io.Ventanas.Enums
         [StringValue("Left 1/3")]
         LeftOneThird,
 
-        [StringValue("Left half")]
+        [StringValue("Left 1/2")]
         LeftHalf,
 
         [StringValue("Left 2/3")]
@@ -16,16 +16,16 @@ namespace Base2io.Ventanas.Enums
         [StringValue("Right 1/3")]
         RightOneThird,
 
-        [StringValue("Right half")]
+        [StringValue("Right 1/2")]
         RightHalf,
 
         [StringValue("Right 2/3")]
         RightTwoThirds,
 
-        [StringValue("Top half")]
+        [StringValue("Top 1/2")]
         TopHalf,
 
-        [StringValue("Bottom half")]
+        [StringValue("Bottom 1/2")]
         BottomHalf,
 
         [StringValue("Center")]
