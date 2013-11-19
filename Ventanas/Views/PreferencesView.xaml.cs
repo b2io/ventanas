3d931d710942545e2f761133c2d69729515d5f7b
tborres <tborres@base2.io>
2013-11-18 22:37:45 -0500

    #1 - Allow the entry of new hotkeys by listening to keyboard events.

--- Ventanas/Views/PreferencesView.xaml.cs
+++ Ventanas/Views/PreferencesView.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Forms;
 using System.Windows.Input;
+using Base2io.Ventanas.Annotations;
 using Base2io.Ventanas.Logic;
 using Base2io.Ventanas.Model;
 using KeyEventArgs = System.Windows.Input.KeyEventArgs;
@@ -13,8 +16,12 @@ namespace Base2io.Ventanas.Views
     /// <summary>
     /// Interaction logic for Preferences.xaml
     /// </summary>
-    public partial class Preferences
+    public partial class Preferences : INotifyPropertyChanged
     {
+        private const string HOTKEY_PROMPT = "Click to enter a new hotkey";
+
+        private PositionHotkey hotkeyEntry;
+
         #region Constructor
 
         public Preferences()
@@ -28,6 +35,21 @@ namespace Base2io.Ventanas.Views
         #region Properties
 
         public ObservableCollection<PositionHotkey> CustomizedHotkeys { get; set; }
+
+        private bool _isHotkeyEntered;
+        public bool IsHotkeyEntered
+        {
+            get { return _isHotkeyEntered; }
+            set
+            {
+                if (value != _isHotkeyEntered)
+                {
+                    _isHotkeyEntered = value;
+                    OnPropertyChanged("IsHotkeyEntered");
+                }
+            }
+        }
+
 
         #endregion
 
@@ -44,8 +66,6 @@ namespace Base2io.Ventanas.Views
             Close();
         }
 
-        #endregion
-
         private void RemoveButtonClick(object sender, RoutedEventArgs e)
         {
             // Get the selected hotkey item.
@@ -59,7 +79,10 @@ namespace Base2io.Ventanas.Views
 
         private void HotkeyEntryBox_OnKeyDown(object sender, KeyEventArgs e)
         {
-            PositionHotkey entry = new PositionHotkey
+            // Mark as handled so that the text control doesn't attempt to handle it.
+            e.Handled = true;
+
+            hotkeyEntry = new PositionHotkey
                 {
                     KeyCode = (Keys)KeyInterop.VirtualKeyFromKey(e.Key),
                     IsCtrlKeyUsed = Keyboard.Modifiers.HasFlag(ModifierKeys.Control),
@@ -68,7 +91,62 @@ namespace Base2io.Ventanas.Views
                     IsWinKeyUsed = Keyboard.Modifiers.HasFlag(ModifierKeys.Windows)
                 };
 
-            HotkeyEntryBox.Text = entry.KeyBindingString;
+            HotkeyEntryBox.Text = hotkeyEntry.KeyBindingString;
+            IsHotkeyEntered = true;
         }
+
+        private void HotkeyList_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ResetHotkeyEntry();
+        }
+
+        private void HotkeyEntryBox_OnInitialized(object sender, EventArgs e)
+        {
+            ResetHotkeyEntry();
+        }
+
+        private void HotkeyEntryBox_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            HotkeyEntryBox.Text = string.Empty;
+        }
+
+        private void Apply_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Get the selected position hotkey.
+            PositionHotkey selectedHotkey = (PositionHotkey)HotkeyList.SelectedItem;
+
+            // Update the selected hotkey to have the new values:
+            selectedHotkey.KeyCode = hotkeyEntry.KeyCode;
+            selectedHotkey.IsCtrlKeyUsed = hotkeyEntry.IsCtrlKeyUsed;
+            selectedHotkey.IsAltKeyUsed = hotkeyEntry.IsAltKeyUsed;
+            selectedHotkey.IsShiftKeyUsed = hotkeyEntry.IsShiftKeyUsed;
+            selectedHotkey.IsWinKeyUsed = hotkeyEntry.IsWinKeyUsed;
+        }
+
+        #endregion
+
+        #region Private Logic
+
+        private void ResetHotkeyEntry()
+        {
+            // Set the prompt text in the hotkey entry box.
+            if (HotkeyEntryBox != null)
+            {
+                HotkeyEntryBox.Text = HOTKEY_PROMPT;
+            }
+            IsHotkeyEntered = false;
+        }
+
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
